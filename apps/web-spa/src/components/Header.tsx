0f277f6da14052cab4c2fdd1e31fb528ca656d18
cd <cdhj996@gmail.com>
2025-07-02 16:20:24 +0800

    refactor(spa): update dark mode styles and improve component aesthetics
    - Changed background colors for dark mode in HomePage, Header, and HowItWorksSection components for better contrast and visual appeal.
    - Updated input and tab styles to enhance user experience in dark mode.
    - Adjusted hover effects and animations for content cards in HowItWorksSection.
    - Improved accessibility by ensuring consistent color usage across components.

--- apps/web-spa/src/components/Header.tsx
+++ apps/web-spa/src/components/Header.tsx
@@ -4,8 +4,8 @@ import { ThemeToggle } from '@/components/ThemeToggle'
 
 export default function Header() {
   return (
-    <header className="relative w-full py-8 z-10 bg-[#0052D9] dark:bg-gray-900 text-white dark:text-gray-200 overflow-hidden">
-      <Lock className="hidden md:block absolute size-34 top-1/3 -left-12 text-[#4c85e4] dark:text-blue-400" />
+    <header className="relative w-full py-8 z-10 bg-[#0052D9] dark:bg-[#0E0F11] text-white dark:text-gray-200 overflow-hidden">
+      <Lock className="hidden md:block absolute size-34 top-1/3 -left-12 text-[#4c85e4] dark:text-[#121212]" />
       <div className="relative max-w-6xl mx-auto flex flex-col md:flex-row items-center md:justify-between gap-4 p-4">
         <div className="flex-1 text-center space-y-2">
           <h1 className="text-3xl md:text-4xl font-bold flex items-center justify-center text-white dark:text-gray-200">
